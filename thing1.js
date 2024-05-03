fae3bc3a4dd875629fe34a71b40517ac72866b10
kouyacha <134472890+ponamaru@users.noreply.github.com>
2024-05-03 15:03:53 +0900

    Update thing1.js

--- thing1.js
+++ thing1.js
@@ -1,12 +1,21 @@
+
+
 document.write( '<img src="目.png" id="thing1" >' );
 
 function stagelink2() {
-document.getElementById( 'thing1' ).style.left = stageX +1400 + "px";
+if(since === 1) {
+document.getElementById( 'thing1' ).style.left = stageX + 1400 + "px";
 document.getElementById( 'thing1' ).style.top = stageY + 400 + "px";
+}
 
+document.getElementById( 'dor1' ).style.left = stageX + dorX + "px";
+document.getElementById( 'dor1' ).style.top = stageY + 230 + "px";
 }
 let intervalId3 = setInterval(stagelink2, 10);
 
+var dorX = 2200;
+
+var fun = 1;
 var array = ['Eye"Hi,I am Isaac."', 'Stick"I am Stick."', 'He is nice guy.','　'];
 
 addEventListener( "keydown", Z );
@@ -44,3 +53,44 @@ var key_code = event.keyCode;
 	document.getElementById("area").innerText = array[nowtext];
         }
 }
+
+document.write( '<img src="dor.png" id="dor1" width="600">' );
+
+addEventListener( "keydown", changestage );
+
+function changestage() {
+var key_code = event.keyCode;
+if( key_code === 90 ) {
+if(Math.sqrt( Math.pow( stageX+dorX-x, 2 ) + Math.pow( stageY+230-y, 2 ) ) < 200) {
+if(fun === 1) {
+backcos();
+backcos2();
+fun = 2;
+stageX = 200;
+dorX = 400;
+document.getElementById( 'thing1' ).style.left = stageX - 800 + "px";
+}
+}
+}
+}
+
+function backcos() {
+    const changeimg = document.getElementById("img1");
+    changeimg.src = "stage2.png";
+}
+function backcos2() {
+    const changeimg = document.getElementById("img2");
+    changeimg.src = "stage2.png";
+}
+
+function backcos3() {
+    const changeimg = document.getElementById("img1");
+    changeimg.src = "teststage.png";
+}
+function backcos4() {
+    const changeimg = document.getElementById("img2");
+    changeimg.src = "teststage.png";
+}
+
+
+
