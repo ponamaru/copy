e02290253196472b5a95645a213e063e06e1b2bf
kouyacha <134472890+ponamaru@users.noreply.github.com>
2024-07-17 14:16:38 +0900

    Update test.js

--- test.js
+++ test.js
@@ -220,7 +220,7 @@ function makita () {
 let intervalmakita2;
 function makita2() {
         y += 5;
-	  if (element.classList.contains("rim")) {
+	  if (element.classList.contains("rlm")) {
 	if(lr === 1) stageX -= 5;
 	if(lr === 2) stageX += 5;
 	  }
